17efbcacc74f70b2ceee1f7cfcba6d788f9827c9
root <1046762075@qq.com>
2020-05-01 00:13:26 +0800

    后端管理页面

--- gmall-manage-web/src/test/java/com/firenay/gmall/GmAllManageWebApplication.java
+++ gmall-manage-web/src/test/java/com/firenay/gmall/GmAllManageWebApplication.java
@@ -8,6 +8,7 @@ import org.csource.fastdfs.TrackerClient;
 import org.csource.fastdfs.TrackerServer;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -17,6 +18,9 @@ import java.io.IOException;
 @SpringBootTest
 public class GmAllManageWebApplication {
 
+	@Value("${fileServer.url}")
+	private String ipAddr;
+
 	@Test
 	public void textFileUpload() throws IOException, MyException {
 		String file = this.getClass().getResource("/tracker.conf").getFile();
@@ -24,15 +28,16 @@ public class GmAllManageWebApplication {
 		TrackerClient trackerClient = new TrackerClient();
 		TrackerServer trackerServer = trackerClient.getTrackerServer();
 		StorageClient storageClient = new StorageClient(trackerServer, null);
-		String orginalFilename = "C:\\Users\\root\\Desktop\\5a38e39b2975e837.jpg";
+		String orginalFilename = "D:\\下载\\谷歌下载\\abe1eea3ca79fc28-c577ebdcb0f3dbcc-b4976fac9ddc9806d92f22e03f1e9b43.jpg";
 		String extName = StringUtils.substringAfterLast(orginalFilename, ".");
 		String[] upload_file = storageClient.upload_file(orginalFilename, extName, null);
 		for (int i = 0; i < upload_file.length; i++) {
 			String s = upload_file[i];
-			System.out.println("s = " + s);
+			ipAddr += "/" + s;
 //			s = group1
 //			s = M00/00/00/CisBNF6oIdqAeWkJAAifB5wykMs818.jpg
 		}
+		System.out.println("访问：" + ipAddr);
 	}
 
 	@Test
