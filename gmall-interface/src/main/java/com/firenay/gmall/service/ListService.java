1bc05f31a81f67a360f6ad8415e6909b67e90f8d
root <1046762075@qq.com>
2020-05-03 23:21:35 +0800

    interface

---
+++ gmall-interface/src/main/java/com/firenay/gmall/service/ListService.java
@@ -0,0 +1,23 @@
+package com.firenay.gmall.service;
+
+import com.firenay.gmall.entity.SkuLsInfo;
+import com.firenay.gmall.entity.SkuLsParams;
+import com.firenay.gmall.entity.SkuLsResult;
+
+/**
+ * <p>Title: ListService</p>
+ * Description：
+ * date：2020/5/2 16:43
+ */
+public interface ListService {
+
+	/**
+	 * 保存数据到ES
+	 */
+	void saveSkuLsInfo(SkuLsInfo skuLsInfo);
+
+	/**
+	 * 检数据
+	 */
+	SkuLsResult search(SkuLsParams skuLsParams);
+}
