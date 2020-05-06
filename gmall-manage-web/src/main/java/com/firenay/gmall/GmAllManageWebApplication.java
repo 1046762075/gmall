bc78ea8dab62e5b7a1ec7a3b265c67c31c8e9b8c
root <1046762075@qq.com>
2020-05-06 17:55:38 +0800

    后端管理页面

--- gmall-manage-web/src/main/java/com/firenay/gmall/GmAllManageWebApplication.java
+++ gmall-manage-web/src/main/java/com/firenay/gmall/GmAllManageWebApplication.java
@@ -2,7 +2,9 @@ package com.firenay.gmall;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+@ComponentScan("com.firenay.gmall")
 @SpringBootApplication
 public class GmAllManageWebApplication {
 	//  本地访问												nginx
